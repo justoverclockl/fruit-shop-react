85b5179a33464da43cf66a20c446fc09665926a4
Marco Colia <info@justoverclock.it>
2022-06-01 10:30:34 +0200

    fixed box

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <div className="grid overflow-hidden md:grid-cols-2  gap-4 w-screen h-screen items-center relative">
             <div className="hidden md:block bg-watermelon h-full w-full bg-cover bg-center"></div>
-            <div className=""></div>
+            <div className="flex justify-center items-center"></div>
             <div className="flex justify-center items-center">
                 <div className="hidden md:block">
                     <h1 className="font-bold text-5xl mb-2 text-amber-500">
@@ -18,7 +18,7 @@ const Home = () => {
                 </div>
             </div>
             <div className="hidden md:block bg-ananas h-full w-full bg-cover bg-center"></div>
-            <div className="absolute inset-x-0 shadow-xl bg-white w-[200px] h-[200px] flex flex-col justify-center items-center mx-auto -mt-1 rounded-lg">
+            <div className="w-[350px] h-[350px] absolute inset-x-0 shadow-xl bg-white md:w-[200px] md:h-[200px] flex flex-col justify-center items-center mx-auto -mt-1 rounded-lg">
                 <img src={MainLogo} alt="main logo" />
                 <Link to="/shop">
                     <button className="relative bottom-8 p-1 bg-amber-500 text-white px-4 rounded-2xl cursor-pointer hover:bg-amber-600">
